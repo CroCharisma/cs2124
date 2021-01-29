2336348ac4dceaa69bd8962d80ba8da774ef8e99
crocharimsa <cjnewco@icloud.com>
2021-01-28 20:59:00 -0600

    initial commit

---
+++ Project0/vbr868Project0.c
@@ -0,0 +1,36 @@
+#include <stdio.h>  //For input/output functions.
+#include <stdlib.h> //For malloc, free, etc.
+#include "Project0.h"  //Include our own header file for the Employee struct.
+
+
+int main()
+{
+	int n = 0;
+	char buffer[50] = "";
+	//1.  Open the file Project0.txt and read in the first line of the file to determine how many Employees we need for our array, and use malloc() to dynamically allocate an array of this size.
+	FILE *infile = fopen( "./project0.txt", "r" );
+	if( infile == NULL ){
+		printf("%s", "ERROR FILE NO OPEN\n");
+		return -1;
+	}
+
+	fscanf(infile,"%s", &n );
+
+	Employee* list = (Employee*)malloc( n * sizeof( Employee ) );
+	
+	//2.  Use a loop to read the Employee information from Project0.txt and write the information to the entries of your array.  Suggestion:  read a single line of the file using fgets() then use sscanf() to parse out the variables from the line.  
+	int i = 0;
+	for( i = 0; i < n; i++ ){
+		fgets( buffer, 50, infile );
+		sscanf( buffer, "%s %d %lf", list[i].name, list[i].ID, list[i].rate);
+	}
+	
+	//3.  Loop through your array and print out the employee information.  Each employee (name, ID, hourly rate) should be on a single line.  The name should be left justified in a column of width 10. 
+	// The IDs are each 6 digits long so they can be printed with a single space after it.  The hourly rate should be printed to 2 decimal places.
+	for( i = 0; i < n; i++ ){
+		printf( "%-10s %d %.2lf", list[i].name, list[i].ID, list[i].rate);
+	}
+
+
+	return 0;
+}
